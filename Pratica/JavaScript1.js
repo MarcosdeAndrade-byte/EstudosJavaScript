ac1b1da63460d81641e476ac56a616548a718039
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-04-13 15:55:00 -0300

    1003 - Soma Simples

--- Pratica/JavaScript1.js
+++ Pratica/JavaScript1.js
@@ -1,17 +1,16 @@
 /*
-A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
-
-- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
 
 Entrada
-A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+O arquivo de entrada contém 2 valores inteiros.
 
 Saída
-Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 */
 
-let area;
-let raio = 2;
+let A = parseInt(lines.shift());
+let B = parseInt(lines.shift());
 
-area = Math.PI * Math.pow(raio,2);
-console.log("A=",area.toFixed(4));
+let soma = A + B;
+
+console.log("SOMA = " + soma)
