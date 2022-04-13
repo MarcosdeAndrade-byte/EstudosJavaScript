3197325489142a2cbb75fbd3ec819e566ab043da
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-04-13 15:47:12 -0300

    1002 Área do Círculo

--- Pratica/JavaScript1.js
+++ Pratica/JavaScript1.js
@@ -1,15 +1,17 @@
 /*
-Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
 
 Entrada
-A entrada contém 2 valores inteiros.
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
 
 Saída
-Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 */
 
-let A,B;
+let area;
+let raio = 2;
 
-A = parseInt(lines.shift());
-B = parseInt(lines.shift()); 
-console.log("X = ",(A+B));
+area = Math.PI * Math.pow(raio,2);
+console.log("A=",area.toFixed(4));
