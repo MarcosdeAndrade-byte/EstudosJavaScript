43fa2643ef411bc498d7877e68be7b5fce68810d
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-04-13 16:55:07 -0300

    1006 Média 2

--- Pratica/JavaScript1.js
+++ Pratica/JavaScript1.js
@@ -1,15 +1,15 @@
 /*
-Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+Leia 3 valores, no caso, variáveis A, B e C, que são as três notas de um aluno. A seguir, calcule a média do aluno, sabendo que a nota A tem peso 2, a nota B tem peso 3 e a nota C tem peso 5. Considere que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
 
 Entrada
-O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+O arquivo de entrada contém 3 valores com uma casa decimal, de dupla precisão (double).
 
 Saída
-Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+Imprima a mensagem "MEDIA" e a média do aluno conforme exemplo abaixo, com 1 dígito após o ponto decimal e com um espaço em branco antes e depois da igualdade. Assim como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 */
 
-let notaA = 5.0,notaB = 7.1;
+let notaA = 10.0,notaB = 10.0,notaC = 5.0;
 
-let media = (notaA * 3.5 + notaB * 7.5) / 11;
+let media = (notaA * 2 + notaB * 3 + notaC * 5) / 10;
 
-console.log(media.toFixed(5))
+console.log("MEDIA = " + media.toFixed(1));
