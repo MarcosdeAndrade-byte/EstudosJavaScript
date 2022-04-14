e9e1950433ab7cd2064add064de7f443edb9c5d1
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-04-14 15:57:31 -0300

    Fazendo novas strings a partir de partes antigas_Exercicio_Documentacao

--- Pratica/JavaScript1.js
+++ Pratica/JavaScript1.js
@@ -1,21 +1,17 @@
 /*
-Filtrando mensagens de saudação
-No primeiro exercício, começaremos com simplicidade - temos várias mensagens de cartão, 
-mas queremos classificá-las para listar apenas as mensagens de Natal. Queremos que você 
-preencha um teste condicional dentro da estrutura if (...), para testar cada string e 
-apenas imprimi-la na lista se for uma mensagem de Natal.
+Corrigindo a capitalização
+Neste exercício, temos os nomes das cidades no Reino Unido, mas a capitalização está toda desarrumada. Nós queremos que você as altere para que elas sejam todas minúsculas, exceto pela primeira letra maiúscula. Uma boa maneira de fazer isso é:
 
-Primeiro pense em como você poderia testar se a mensagem em cada caso é uma mensagem de Natal. Qual string está presente em todas essas mensagens e que método você poderia usar para testar se ela está presente?
-
-Em seguida, você precisará escrever um teste condicional do formulario operando2 operador operando1. A coisa à esquerda é igual à coisa à direita? Ou neste caso, o método chama à esquerda retorna o resultado à direita?
-
-Dica: Nesse caso, é provavelmente mais útil testar se a chamada do método não é igual a um determinado resultado.
+Converta toda a cadeia contida na variável de entrada para minúscula e armazene-a em uma nova variável.
+Pegue a primeira letra da string nesta nova variável e armazene-a em outra variável.
+Usando esta última variável como substring, substitua a primeira letra da string em minúsculas pela primeira letra da string em minúsculas alterada para maiúscula. Armazene o resultado desse procedimento de substituição em outra nova variável.
+Altere o valor da variável  result para igual ao resultado final, não a input.
 */
 
-let mensagens = ["Happy Birthday!","Merry Christmas my love","A happy Christmas to all the family","You're all I want for Christmas","Get well soon"];
+var mensagens = ["lonDon","ManCHESTer","BiRmiNGHAM","liVERpoOL"];
 
-for(let index = 0;index < mensagens.length;index++){
-    if(mensagens[index].indexOf("Christmas") != -1){
-       console.log(mensagens[index]);
-    }
+for(let i = 0;i < mensagens.length;i++){
+    let minuscula = mensagens[i].toLowerCase();
+    let maiuscula = mensagens[i].toUpperCase();
+    console.log(maiuscula.charAt(0) + minuscula.slice(1));
 }
